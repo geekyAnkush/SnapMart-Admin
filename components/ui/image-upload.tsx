9200b2ad3e4762053f259ff7559a2d259e03a8e7
Kumar Ankush <54018647+geekyAnkush@users.noreply.github.com>
2023-07-30 01:21:05 +0530

    changed image upload preset

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="hgibhy6w">
         {({ open }) => {
           const onClick = () => {
             open();
